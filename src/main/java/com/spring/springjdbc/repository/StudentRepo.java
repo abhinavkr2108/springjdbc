0a2a4a93f22fd666722d6fc77bdc673d0c5fd80f
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-07-05 13:50:55 +0530

    Fetching Data from H2 database using RowMapper

--- src/main/java/com/spring/springjdbc/repository/StudentRepo.java
+++ src/main/java/com/spring/springjdbc/repository/StudentRepo.java
@@ -3,8 +3,11 @@ package com.spring.springjdbc.repository;
 import com.spring.springjdbc.models.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,8 +31,18 @@ public class StudentRepo {
     }
 
     public List<Student> findAll() {
-        List<Student> studentList = new ArrayList<>();
-        return studentList;
+        String sql = "select * from student";
+        RowMapper<Student> rowMapper = new RowMapper<Student>() {
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Student student = new Student();
+                student.setRollNo(rs.getInt("rollNo"));
+                student.setName(rs.getString("name"));
+                student.setMarks(rs.getInt("marks"));
+                return student;
+            }
+        };
+        return jdbc.query(sql,rowMapper);
     }
 
 
